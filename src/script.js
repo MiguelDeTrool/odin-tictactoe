d4a30e1a1cfb163964f1d68617c9e4ae57a33ab5
MiguelDeTrool <migueldetrool@gmail.com>
2022-05-24 00:08:23 +0200

    Add main display update method, modify basic styling

--- src/script.js
+++ src/script.js
@@ -1,26 +1,60 @@
 const displayController = (function() {
-    // let grid = document.querySelectorAll();  Need to define selector
+    const _gridDisplay = document.querySelectorAll(".space");
+    const _playerDisplay = document.querySelector("player-display");
+
+    _gridDisplay.forEach((element) => {
+        element.addEventListener('click', () => {
+            element.getAttribute("data-attribute");
+        });
+    });
+
+    const _updateGrid = (currentGrid) => {
+        for (let i = 0; i < _gridDisplay.length; i++) {
+            _gridDisplay[i].textContent = currentGrid[i];
+        }
+    };
+
+    const _updatePlayer = (currentPlayer) => {
+        _playerDisplay.textContent = `Player ${currentPlayer.number}'s turn, (${currentPlayer.mark})`
+        //make an element's textContent say "Player 1's turn (X)" or opposite
+    };
+
+    const updateDisplay = () => {
+        _updateGrid(gameBoard.returnGrid());
+        _updatePlayer("????");
+    };
 
     return {
-
+        updateDisplay,
     };
 })();
 
 const gameBoard = (function() {
-    _grid = new Array(9);
-    let playMove = function(player, space) {
+    const _grid = new Array(9);
+    // const _grid = ["O", "X", "O", "X", "O", "O", "X", "O", "X",];
+
+    const playMove = (player, space) => {
         let currentMark = player.mark;
         _grid[space] = currentMark;
     };
 
+    const returnGrid = () => {
+        return _grid;
+    };
+
     return {
         playMove,
+        returnGrid
     };
 })();
 
-const Player = function(name, mark) {
+const Player = function(name, number, mark) {
 
     return {
-
+        name,
+        number,
+        mark,
     };
 };
+
+// displayController.updateDisplay()
