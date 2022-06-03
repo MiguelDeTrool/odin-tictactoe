8359973d860b1e7b4bef53961364f3c6a84b6af4
MiguelDeTrool <migueldetrool@gmail.com>
2022-06-03 23:21:34 +0200

    Add check win function

--- src/script.js
+++ src/script.js
@@ -36,41 +36,71 @@ const gameBoard = (function() {
         return _grid;
     };
 
-    const checkWin = () => {
-        // Check horizontal win (%1)
-        // Check vertcal win (%3)
-        // Check diagonal win (special cases) 
-
-    }
 
     return {
         playMove,
         returnGrid,
-        checkWin,
     };
 })();
 
 const turnHandler = (function() {
     const _gridElements = document.querySelectorAll(".space");
+
     let _currentPlayer = {};
     let _nextPlayer = {};
+
+    const _checkWin = () => {
+        let grid = gameBoard.returnGrid();
+
+        // Check horizontal win (%1)
+        for (let i = 0; i < 7; i = i + 3) { // Increment by one to check each column
+            if (grid[i] != undefined && grid[i] === grid[i + 1] && grid[i] === grid[i + 2]) { // Checks the first space of the row isn't empty, then checks the other spaces in the row are filled with the same mark
+                return true;
+            }
+        }
+
+        // Check vertical win (%3)
+        for (let i = 0; i < 3; i++) { // Increment by one to check each column
+            if (grid[i] != undefined && grid[i] === grid[i + 3] && grid[i] === grid[i + 6]) { // Checks the first space of the column isn't empty, then checks the other spaces in the column are fllled with the same mark
+                return true;
+            }
+        }
+
+        // Check diagonal win (special cases) 
+        if (grid[0] != undefined && grid[0] === grid[4] && grid[0] === grid[8]) {
+            return true;
+        }
+
+        if (grid[2] != undefined && grid[0] === grid[4] && grid[0] === grid[6]) {
+            return true;
+        }
+    }
 
     // Event listener for when a turn is played, update model, then display
     const playTurn = function (event) {
             let playedSpace = event.target.getAttribute("data-attribute");
 
-            //Check space is empty before playing turn
+            //Check space is empty before playing move
             if (gameBoard.returnGrid()[playedSpace] === undefined) {
                 gameBoard.playMove(_currentPlayer, playedSpace);
+
+                // Check win
+                if (_checkWin() === true) {
+                    displayController.updateDisplay();
+                    alert(`${_currentPlayer.name} has won!`);
+                    // Modal needed to offer to play again
+                    return;
+                }
+
                 // Update current and next players
                 let tempPlayer = _currentPlayer;
                 _currentPlayer = _nextPlayer;
                 _nextPlayer = tempPlayer;
 
                 // Update display
-                displayController.updateDisplay()
+                displayController.updateDisplay();
 
-            //Then check win
+                //Then check if any more spaces to determine draw
 
             } else {
                 alert("Space not free");
